ad8cb5263fde8f4b0a9a7e83f1b487b1aab51da9
Higor Paulo <h.pauloc12@gmail.com>
2026-05-31 00:06:47 -0300

    chore: adiciona configuracao, classe principal e teste de contexto

---
+++ src/main/java/br/com/importaai/ecommercepetshop/EcommercePetshopApplication.java
@@ -0,0 +1,13 @@
+package br.com.importaai.ecommercepetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePetshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/importaai/ecommercepetshop/EcommercePetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.importaai.ecommercepetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
